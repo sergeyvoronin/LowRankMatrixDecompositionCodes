3e6aef85de9b729c626c4151cdced77f49076e45
sergey voronin <sergey@localhost.localdomain>
2022-10-18 22:32:35 -0400

    updates and TOL based driver

--- oneapi_code/compile.sh
+++ oneapi_code/compile.sh
@@ -6,3 +6,6 @@ icx -m64 -mkl -openmp -I"${MKLROOT}/include" -I"${MKLROOT}/examples/c/blas/sourc
 
 
 icx -m64 -mkl -openmp -I"${MKLROOT}/include" -I"${MKLROOT}/examples/c/blas/source/" driver2.c matrix_vector_functions_one_api.c rank_revealing_algorithms_one_api.c -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl -o rund2
+
+
+icx -m64 -mkl -openmp -I"${MKLROOT}/include" -I"${MKLROOT}/examples/c/blas/source/" driver3.c matrix_vector_functions_one_api.c rank_revealing_algorithms_one_api.c -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl -o rund3
